73e924013177a2e2939d41186789a0e433fd8389
twbro54l <twbro54l@users.noreply.github.com>
2024-08-28 18:42:30 +0300

    Add project files.

---
+++ Schimb/Schimb.cpp
@@ -0,0 +1,39 @@
+// Schimb.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float a, b, aux;
+	cout << "VAL A = ";
+	cin >> a;
+	if (!cin) {
+		cout << "nu este valid" << endl;
+		return -1;
+	}
+	cout << "VAL B = ";
+	cin >> b;
+	if (!cin) {
+		cout << "nu este valid" << endl;
+		return -1;
+	}
+	aux = a;
+	a = b;
+	b = aux;
+	cout << "A = " << a << "; B = " << b << endl;
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
